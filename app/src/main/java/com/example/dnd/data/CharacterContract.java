44bda3a0a2eba39e7396d39a2fdf2690f85a6473
atsuko_c <atsukosgit@gmail.com>
2019-02-27 20:15:08 -0700

    added test method

--- app/src/main/java/com/example/dnd/data/CharacterContract.java
+++ app/src/main/java/com/example/dnd/data/CharacterContract.java
@@ -1,4 +1,18 @@
 package com.example.dnd.data;
 
-public class CharacterContract {
+import android.provider.BaseColumns;
+
+public final class CharacterContract {
+
+        public static final class CharacterEntry implements BaseColumns{
+
+            public static final String table_name = "charactersTable";
+            public static final String _id = BaseColumns._ID;
+            public static final String name_col = "name";
+            public static final String characterAttacksId_col = "characterAttacksId";
+
+        }
 }
+
+
+
