0e2358e8aeec2024ddee1e2ee77768354154b23d
Aaron Lee <lee16020@byui.edu>
2019-02-27 20:27:35 -0700

    Adding new activities to the System

---
+++ app/src/main/java/com/example/dnd/AttackAddEdit.java
@@ -0,0 +1,13 @@
+package com.example.dnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AttackAddEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attack_add_edit);
+    }
+}
