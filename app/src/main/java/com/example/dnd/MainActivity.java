60a99eef8541f343b4e7cb67915f04888826e8e7
Zuffle <8173816+Zuffle@users.noreply.github.com>
2019-03-06 19:35:20 -0700

    sharepreferences now saves the targetAC from MainActivity xml

--- app/src/main/java/com/example/dnd/MainActivity.java
+++ app/src/main/java/com/example/dnd/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dnd;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
 
 import com.example.dnd.data.AttackDatabaseHelper;
 import com.example.dnd.data.AttackDiceDatabaseHelper;
@@ -9,6 +12,7 @@ import com.example.dnd.data.CharacterAttacksDatabaseHelper;
 import com.example.dnd.data.CharacterDatabaseHelper;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     CharacterDatabaseHelper characterDbHelper;
     AttackDiceDatabaseHelper attackDiceDbHelper;
@@ -41,5 +45,27 @@ public class MainActivity extends AppCompatActivity {
         characterAttacksDbHelper.addCharacterAttack(1, 2);
 
         setContentView(R.layout.activity_main);
+    }
+
+    public int roll(int AC) {
+        // get the text view from the View to get the user input
+        EditText textAC = findViewById(R.id.targetACEditText);
+
+        // instantiate the objects to open and manipulate the share preferences file
+        SharedPreferences sPref = getSharedPreferences("com.example.dnd_prefs", MODE_PRIVATE);
+        SharedPreferences.Editor editPref = sPref.edit();
+
+        // add the string with AC to the shared preferences file
+        editPref.putString("targetAC", textAC.toString());
+
+        // apply the changes to the sharedPreferences file
+        editPref.apply();
+
+        Log.d(TAG, "Target AC saved to sharedpref file \"com.example.dnd_prefs\"");
+
+        //TODO: run the roll methods from the attack set object
+
+        return -1;
+
     }
 }
