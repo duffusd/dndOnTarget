24169232cd5d3cd1e520a8e945a97363d34ad35c
admin <atsukoc@gmail.com>
2019-03-07 21:56:23 -0700

    Fixed addName method in CharacterAddEdit.java

--- app/src/main/java/com/example/dnd/CharacterAddEdit.java
+++ app/src/main/java/com/example/dnd/CharacterAddEdit.java
@@ -1,5 +1,6 @@
 package com.example.dnd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,37 +12,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import com.example.dnd.data.CharacterDatabaseHelper;
 
-public class CharacterAddEdit extends AppCompatActivity {
+public class CharacterAddEdit extends AppCompatActivity implements View.OnClickListener {
+
     CharacterDatabaseHelper myDB;
-    public Button btnAdd;
-    EditText editText;
+    Button addEditButton;
+    EditText nameEditTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the view
         setContentView(R.layout.activity_character_add_edit);
+
+        // Set the Add/Edit button
+        addEditButton = this.findViewById(R.id.addEditCharacterButton);
+        addEditButton.setOnClickListener(this);
+
+        // Set the Name fieldname
+        nameEditTextField = this.findViewById(R.id.characterNameEditText);
+
+        // Initiate the db helper
         myDB = new CharacterDatabaseHelper(this);
 
-        editText = (EditText) findViewById(R.id.characterNameEditText);
-        btnAdd = (Button) findViewById(R.id.saveCharacterButton);
-
-
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String newEntry = editText.getText().toString();
-                if(editText.length()!= 0){
-                    AddData(newEntry);
-                    editText.setText("");
-                }else{
-                    Toast.makeText(CharacterAddEdit.this, "You must put something in the text field!", Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
     }
-    public void AddData(String newEntry) {
+
+    public void addName(String newEntry) {
 
         boolean insertData = myDB.addName(newEntry);
 
@@ -52,7 +48,14 @@ public class CharacterAddEdit extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.addEditCharacterButton:
+                String newCharacterName = nameEditTextField.getText().toString();
+                addName(newCharacterName);
+                break;
 
-
-
+        }
+    }
 }
