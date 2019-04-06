ab6e0ed54acd2c911b467a8f3af24deb83ab9d92
admin <atsukoc@gmail.com>
2019-04-05 19:45:37 -0600

    Updated javadoc and cleaned up the code

--- app/src/main/java/com/example/dnd/data/DatabaseContract.java
+++ app/src/main/java/com/example/dnd/data/DatabaseContract.java
@@ -1,5 +1,9 @@
 package com.example.dnd.data;
 
+/**
+ * DatabaseContract class contains the constant definitions for the database used for the app
+ *
+ */
 public class DatabaseContract {
 
     public static final int version = 3;
